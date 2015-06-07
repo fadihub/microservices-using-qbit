da5d0f95ac0f2939857d776ed7e34a3c1ff137ba
fadihub <fadimaalouli@gmail.com>
2015-06-07 12:45:53 -0700

    version 0.8.6

--- src/main/java/io/advantageous/qbit/example/Main.java
+++ src/main/java/io/advantageous/qbit/example/Main.java
@@ -1,13 +1,13 @@
 package io.advantageous.qbit.example;
 
-import io.advantageous.qbit.server.ServiceServer;
-import io.advantageous.qbit.server.ServiceServerBuilder;
+import io.advantageous.qbit.server.EndpointServerBuilder;
+import io.advantageous.qbit.server.ServiceEndpointServer;
 
 public class Main {
 
     public static void main(String... args) {
-        final ServiceServer serviceServer = ServiceServerBuilder
-                .serviceServerBuilder()
+        final ServiceEndpointServer serviceServer = EndpointServerBuilder
+                .endpointServerBuilder()
                 .setUri("/main")
                 .setPort(9090).build();
         serviceServer.initServices(
